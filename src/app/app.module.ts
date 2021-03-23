7b0746360847e89ee5631b5c18b1462845e2a533
theylkszaszin <csfelipe8@gmail.com>
2021-03-23 20:14:23 -0300

    Resolvido o problema do material: Inserido em Import ao inves de declarations. Realizado a introduação de ligação de propriedades e interação em ngOnInit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
 import { PropertyBindingComponent } from './property-binding/property-binding.component';
-// import {MatButtonModule} from '@angular/material/button';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     StringInterpolationComponent,
-    PropertyBindingComponent,
-    // MatButtonModule
+    PropertyBindingComponent
   ],
   imports: [
+    MatButtonModule,
     BrowserModule,
     BrowserAnimationsModule
   ],
--- src/app/property-binding/property-binding.component.ts
+++ src/app/property-binding/property-binding.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyBindingComponent implements OnInit {
 
+  colors: Array<string> = ["primary","accent", "warn", ""];
+  colorprimary: string = "primary";
+  colorwarn: string = "warn";
+  idx = 0;
+
+  btnDisabled = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.idx === this.colors.length-1 ? this.idx = 0 : this.idx += 1 % this.colors.length;
+    }, 2000);
   }
 
 }

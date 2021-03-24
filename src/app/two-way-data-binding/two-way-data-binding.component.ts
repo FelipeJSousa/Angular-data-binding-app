0a937491ed92d8ac3e7dd62bd51cc7fa33947569
theylkszaszin <csfelipe8@gmail.com>
2021-03-23 23:40:33 -0300

    TwoWayDataBindingComponent part 2

--- src/app/two-way-data-binding/two-way-data-binding.component.ts
+++ src/app/two-way-data-binding/two-way-data-binding.component.ts
@@ -10,6 +10,13 @@ export class TwoWayDataBindingComponent implements OnInit {
   name1: string = "name1";
   name2: string = "name2";
 
+  client = {
+    firstName: "John",
+    lastName: "Michael",
+    address: "Avenue St. Jonny",
+    age: 49
+  }
+
   constructor() { }
 
   ngOnInit(): void {
